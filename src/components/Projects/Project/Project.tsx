901e5c59e9105118c02b5bdd2af3be54caaa8db9
AlexndrG <alexndr1@mail.ru>
2021-12-03 11:08:47 +0300

    Верстка Лекция 2 (sass)

--- src/components/Projects/Project/Project.tsx
+++ src/components/Projects/Project/Project.tsx
@@ -1,20 +1,27 @@
 import React from 'react';
-import s from './Project.module.css'
+import s from './Project.module.scss'
 
 type PropjectPropsType = {
     title: string
     description: string
+    style: {backgroundImage: string}
 }
 
 export const Project = (props: PropjectPropsType) => {
     return (
-        <div className={s.propject}>
+        <div className={s.project}>
             <a href={''}>
-                <div className={s.picture}></div>
+                <div
+                    className={s.picture}
+                    style={props.style}
+                >
+                </div>
             </a>
-            <div className={s.propjectContainer}>
-                <h3 className={s.projectTitle}>{props.title}</h3>
-                <div className={s.description}>{props.description}</div>
+            <div className={s.projectContainer}>
+                <div className={s.projectInfo}>
+                    <h3 className={s.projectTitle}>{props.title}</h3>
+                    <div className={s.description}>{props.description}</div>
+                </div>
             </div>
         </div>
     )
--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
-import s from './Projects.module.css'
+import s from './Projects.module.scss'
 import sContainer from '../../common/styles/container.module.css'
 import {Project} from './Project/Project';
 import {Title} from '../../common/components/title/Title';
 
+import socialImage from '../../assets/image/social.png'
+import todoImage from '../../assets/image/todo.png'
 
 export const Projects = () => {
+    const social = {
+        backgroundImage: `url(${socialImage})`,
+    }
+    const todo = {
+        backgroundImage: `url(${todoImage})`,
+    }
+
     return (
         <div className={s.projectsBlock}>
             <div className={`${sContainer.container} ${s.projectsContainer}`}>
@@ -13,10 +22,12 @@ export const Projects = () => {
 
                 <div className={s.projects}>
                     <Project
+                        style={social}
                         title={'Social Network'}
                         description={'sdj vp98j rtglk sdflk vbsp[ritj h;lkrhp;09 erltkh rh'}
                     />
                     <Project
+                        style={todo}
                         title={'TodoList'}
                         description={'sdj vp98j rtglk sdflk vbsp[ritj h;lkrhp;09 erltkh rh sdj vp98j rtglk sdflk vbsp[ritj h;lkrhp;09 erltkh rh'}
                     />
